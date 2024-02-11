89669ca38e5e4841abb3c8b336f2f13370b546ab
Mike Locks <mikeloc@hotmail.fr>
2024-02-11 15:47:57 +0100

    Affichage des projets en utilisant l'api /works + creation de fichiers consts & functions.js pour factoriser le projet une fois celui-ci termine

---
+++ Backend/functions.js
@@ -0,0 +1,16 @@
+
+
+const filterByCategory = () => {
+    // TODO FUNCTION TO FILTER CAT
+}
+
+const fetchLoginToken = () => {
+
+}
+
+const saveLoginToken = () => {
+
+}
+
+// TODO FUNCTIONS
+
--- Backend/projets.js
+++ Backend/projets.js
@@ -1,7 +1,17 @@
-const reponse = await fetch("projets-design.json")
-const projets = await reponse.json();
+const API_PREFIX = "http://localhost:5678/api/"
 
+async function chargerProjets() {
+    const reponse = await fetch(API_PREFIX + "works");
+    const projets = await reponse.json();
+    return projets;
+  }
+  
+  async function initialiserPage() {
+    const projets = await chargerProjets();
+    genererProjet(projets);
+  }
 
+function genererProjet(projets){
 for (let i = 0; i < projets.length; i++) {
 
     const figure = projets[i];
@@ -10,13 +20,17 @@ for (let i = 0; i < projets.length; i++) {
     projetElement.dataset.id = projets[i].id;
     
     const imageElement = document.createElement("img");
-    imageElement.src = figure.img;
+
+    imageElement.src = figure.imageUrl;
     const descriptionElement = document.createElement("figcaption");
-    descriptionElement.innerText = figure.description;
+    descriptionElement.innerText = figure.title;
 
-
+    
     sectionGallery.appendChild(projetElement);
     projetElement.appendChild(imageElement);
     projetElement.appendChild(descriptionElement)
+    }
 }
 
+// Appel de la fonction pour initialiser la page
+initialiserPage();
