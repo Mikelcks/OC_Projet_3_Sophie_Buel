c47006d54dcc7f834d06e2627b80aacc564bdbbc
Mike Locks <mikeloc@hotmail.fr>
2024-02-12 09:50:47 +0100

    Mise en place des filtres sur les projets

--- Backend/projets.js
+++ Backend/projets.js
@@ -9,6 +9,46 @@ async function chargerProjets() {
   async function initialiserPage() {
     const projets = await chargerProjets();
     genererProjet(projets);
+
+        const boutonTrierTous = document.getElementById("btnTous");
+        boutonTrierTous.addEventListener("click", function () {
+            const tous = projets.filter(function (projet) {
+            return projet.category.id;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererProjet(tous)
+            console.log(tous);
+        });
+
+        const boutonTrierObjets = document.getElementById("btnObjets");
+        boutonTrierObjets.addEventListener("click", function () {
+            const objets = projets.filter(function (projet) {
+                return projet.category.id === 1;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererProjet(objets)
+            console.log(objets);
+        });
+
+        const boutonTrierAppartements = document.getElementById("btnAppartements");
+        boutonTrierAppartements.addEventListener("click", function () {
+            const appartements = projets.filter(function (projet) {
+                return projet.category.id === 2;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererProjet(appartements)
+            console.log(appartements);
+        });
+
+        const boutonTrierHotels_Restaurants = document.getElementById("btnHotels_Restaurants");
+        boutonTrierHotels_Restaurants.addEventListener("click", function () {
+            const Hotels_Restaurants = projets.filter(function (projet) {
+                return projet.category.id === 3;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            genererProjet(Hotels_Restaurants)
+            console.log(Hotels_Restaurants);
+        });
   }
 
 function genererProjet(projets){
@@ -34,3 +74,4 @@ for (let i = 0; i < projets.length; i++) {
 
 // Appel de la fonction pour initialiser la page
 initialiserPage();
+
